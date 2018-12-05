44959e294a12abcd3086e83a68a95693ecea8877
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-12-05 14:27:33 +0000

    Automatically backed up by Learn

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,17 @@
+class ArtistsController < ApplicationController
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+end
